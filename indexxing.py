b190c4393c86188f9fd87afa666e1eb800fca635
Eric Antony <ericantony93@gmail.com>
2025-06-27 09:55:20 +0530

    samples

---
+++ indexxing.py
@@ -0,0 +1,39 @@
+# a="Hello World"
+# print(a[0:5])
+
+# a="Hello World"
+# print(a[0:9:2])
+
+# a="Hello World"
+# print(a[::-1])
+
+# a="Hello World"
+# print(a[::-1])
+# a[1]="y"
+# print(a)
+
+#string functions and methods
+#lowercase
+# message="PYTHON IS FUN"
+# print(message.lower())
+
+#uppercase
+# message="python is fun"
+# print(message.upper())
+
+#count
+# txt="i love apples,apples are my favourite fruit"
+# x=txt.count("p")
+# print(x)
+
+#find
+# message="python is a fun programming language"
+# print(message.find("fun"))
+
+#replace
+# text="Bat Ball"
+# replaced_text=text.replace("Ba","Ro")
+# print(replaced_text)
+
+
+
--- loop.py
+++ loop.py
@@ -132,11 +132,11 @@
 #     print()
 
 #e
-# n=5
+# n=6
 # for i in range(0,5):
 #     for j in range(0,n-1):
-#         if(i==0 or i==2 or i==4 or j==0 or j==4 or j==4):
-#              print(" ",end="*")
+#         if(i==0 or i==2 or i==4  or j==0  or j==5):
+#              print(" *",end="")
 #         else:
 #             print("",end="")
 #     print()
@@ -176,3 +176,90 @@
 #         else:
 #             print(" ",end=" ")
 #     print()
+
+#inverted hollow pyramid
+# n = 5
+# for i in range (0,n):
+#     for j in range(0,i+1):
+#         print(" ",end="")
+
+#     for k in range(0,n-i):
+#         if(i==0 or k==0 or k== n - i - 1 ):
+#             print("",end=" *")
+#         else:
+#             print(" ",end=" ")
+#     print()
+
+#while loop
+# choice=0
+# res=0
+# while True:
+#     print("1.ADDITION")
+#     print("2.SUBSTRACTION")
+#     print("3.MULTIPLICATION")
+#     print("4.DIVISION")
+#     print("5.EXIT")
+#     choice = int(input("enter your choice:"))
+    
+#     if choice==1:
+#         a=int(input("enter your number:"))
+#         b=int(input("enter  your number:"))
+#         res=a+b 
+#     elif choice==2:
+#         a=int(input("enter your number:"))
+#         b=int(input("enter  your number:"))
+#         res=a-b
+#     elif choice==3:
+#         a=int(input("enter your number:"))
+#         b=int(input("enter  your number:"))
+#         res=a*b
+#     elif choice==4:
+#         a=int(input("enter your number:"))
+#         b=int(input("enter  your number:"))
+#         if b==0:
+#             print("invalid")
+#         else:
+#             res=a/b
+#     elif choice==5:
+#         break
+#     else:
+#         print("invalid choice")
+#     break
+#     print(res)
+    
+#registration
+# choice=0
+# name=0
+# age=0
+# address=0
+# username=0
+# password=0
+# usrname=0
+# pass2=0
+# phone=0
+# while True:
+#     print("1.REGISTRATION")
+#     print("2.LOGIN")
+#     choice=int(input("enter your choice: "))
+#     if choice==1:
+#         name=(input("Enter your name:"))
+#         age=(input("Enter your age:"))
+#         address=(input("Enter your address:"))
+#         username=(input("Enter your username:"))
+#         password=(input("Enter your password:"))
+#         phone=str(input("enter your phone number:"))
+#         if len(phone)==10:
+#             break
+#         else:
+#             print("Invalid")
+#     elif choice==2:
+#         usrname=(input("Enter the username:"))
+#         pass2=(input("Enter the password:"))
+#     if usrname==username and pass2==password and int(age)>=18 and len(phone)==10:
+#             print("Name: ",name)
+#             print("Address:",address)
+#             print("Age:",age)
+#             print("Phone:",phone)
+#     else:
+#          print("Invalid") 
+
